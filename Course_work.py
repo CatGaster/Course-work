4849f6b3071895af2e973655ae6609f092f57692
Kotiev Timur <nick.hoult@mail.ru>
2023-10-30 17:41:17 +0300

    v1.3 del Yandex library

--- Course_work.py
+++ Course_work.py
@@ -1,22 +1,24 @@
 import json
 import requests
-import yadisk
+
+# app_id=51774439
+# vk_oauth_url= "https://oauth.vk.com/authorize"
+# params_dict={
+#     "client_id": app_id,
+#     "redirect_uri": "https://oauth.vk.com/blank.html",
+#     "display": "page",
+#     "scope": "photos",
+#     "response_type": "token",
+# }
 
 
-app_id=51774439
-vk_oauth_url= "https://oauth.vk.com/authorize"
-params_dict={
-    "client_id": app_id,
-    "redirect_uri": "https://oauth.vk.com/blank.html",
-    "display": "page",
-    "scope": "photos",
-    "response_type": "token",
-}
+# oauth_url =f'{vk_oauth_url}?{urlencode(params_dict)}'"
+
 token = ""
 
 
 class VKAPIClient:
-    
+
     api_url = "https://api.vk.com/method/"
 
     def __init__(self, token, user_id):
@@ -33,18 +35,18 @@ class VKAPIClient:
     def get_photos(self,):
         method_params = self.common_params()
         method_params.update({"owner_id": self.user_id, "count": self.count,
-                            "rev": self.rev, "album_id": "profile", "extended": "likes",
-                            "photo_sizes": "o", })
+                               "rev": self.rev, "album_id": "profile", "extended": "likes",
+                                 "photo_sizes": "o", })
         response = requests.get(f"{self.api_url}photos.get", params=method_params).json()
         return response
     
-    def get_count(self,count):
+    def get_count(self, count):
         self.count = count
         method_params = self.common_params()
         method_params.update({"count": self.count})
         
     def get_rev(self,rev):
-        self.rev = rev
+        self.rev=rev
         method_params = self.common_params()
         method_params.update({"rev": self.rev})
 
@@ -53,28 +55,38 @@ def upload():
         
     client = VKAPIClient(token,input("Введите VK id : "),)
     y_token = input("Введите Яндекс OAuth Токен: ")
+    
+    headers = {"authorization": y_token}
+    params = {"path": "VK_image", }
+    response = requests.put("https://cloud-api.yandex.net/v1/disk/resources", headers=headers, params=params)
 
     client.get_rev(int(input("Введите Порядок (0 Хронологический) (1 Антихронологический) : ")))
     client.get_count(int(input("Введите количество фото : ")))
+
     for photo in client.get_photos()["response"]["items"]:
-        vk_save_photo = photo["sizes"][-1]["url"]
+        vk_save_photo=photo["sizes"][-1]["url"]
         like = f"{photo["likes"]["count"]} like "
         id_=f'{photo["id"]} id фото;'
 
-        with open("vk_save_photo.json", "w") as file:
+        with open ("vk_save_photo.json", "w") as file:
             json.dump(vk_save_photo, file)
     
         with open("vk_save_photo.json", "r") as file:
             vk_save_photo = json.load(file)
-            response= requests.get(vk_save_photo)
+            response = requests.get(vk_save_photo)
 
-            with open(like + id_ + ".jpg","wb") as f1:
+        with open(like + id_ + ".jpg","wb") as f1:
                 f1.write(response.content)
 
-            yandex_token = yadisk.YaDisk(token = y_token)
-            yandex_token.upload(like + id_ +".jpg", like + id_ +".jpg")
-            
-            print(vk_save_photo, like, id_)
+        param={"path": "VK_image/"+ like + id_  + ".jpg"}
+        response=requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload", params=param, headers=headers)
+        url_upload = response.json().get("href")
+
+        with open(like + id_ + ".jpg", "rb") as file:
+            response = requests.put(url_upload, files = {"file": file})
+
+        print(vk_save_photo, like, id_)
+
 
 if __name__ == "__main__":
     upload()
